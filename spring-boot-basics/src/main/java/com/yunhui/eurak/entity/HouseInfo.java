c5afa9aa8ab427d304d217565e7405c86ed64449
wb_hd04 <wb_hd04@dahuatech.com>
2021-03-04 10:05:45 +0800

    ###springboot基础模块

---
+++ spring-boot-basics/src/main/java/com/yunhui/eurak/entity/HouseInfo.java
@@ -0,0 +1,15 @@
+package com.yunhui.eurak.entity;
+
+import lombok.Data;
+
+@Data
+public class HouseInfo {
+
+    private Long id;
+
+    private String city;
+
+    private String region;
+
+    private String name;
+}
--- springcloud-eurak-client01/src/main/java/com/yunhui/eureka01/controller/HouserController.java
+++ springcloud-eurak-client01/src/main/java/com/yunhui/eureka01/controller/HouserController.java
@@ -14,4 +14,7 @@ public class HouserController {
     {
          return  "hello";
     }
+
+
+
 }
